f1064fd1ef54412dd58f4f1984bd02d11be912ea
romero-eng <39012010+romero-eng@users.noreply.github.com>
2019-03-12 14:08:39 -0400

    Create temporary_output_searcher.py

---
+++ temporary_output_searcher.py
@@ -0,0 +1,36 @@
+import glob
+import numpy as np
+
+output_directory = '/home/jmr95/patientSimulator'
+error_file_names = '/*.err'
+
+error_file_names = output_directory + error_file_names
+output_file_names = []
+error_file_names = glob.glob(error_file_names)
+costs = []
+
+for error_file_name in error_file_names:
+    try:
+        with open(error_file_name, 'r') as error_file:
+            if( error_file.readline() == 'srun: Job step aborted: Waiting up to 62 seconds for job step to finish.\n'):
+                [file_name, file_extension] = error_file_name.split('.')
+                output_file_name = file_name + '.out'
+                output_file_names.append(output_file_name)
+    except IOError as exc:
+        print(exc)
+
+for output_file_name in output_file_names:
+    try:
+        with open(output_file_name, 'r') as output_file:
+            lines = output_file.readlines()
+            lines.reverse()
+            lines = lines[0:10]
+            lines.reverse()
+            line1 = lines[2]
+            line2 = lines[8]
+            cost = float(line1.split(',')[4].split(']')[0])
+            costs.append(cost)
+    except IOError as exc:
+        print(exc)
+
+print(np.argmin(costs))
