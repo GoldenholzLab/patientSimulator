d3e957771abb25045c1566bb3fff0e53632c52ef
romero-eng <39012010+romero-eng@users.noreply.github.com>
2019-05-07 12:41:54 -0400

    made fully fcuntional undocumented plottig code

--- plot_trial.py
+++ plot_trial.py
@@ -3,9 +3,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 import textwrap
+import sys
 
 
-def load_endpoint_from_trial(endpoint_file_name, trial_ID_num):
+def load_data_point_from_trial(endpoint_file_name, trial_ID_num):
+    '''
+
+    Inputs:
+
+        1) endpoint_file_name:
+
+            (string) - the file name of the 
+
+    Outputs:
+
+    '''
 
     with open( os.getcwd() + '/' + endpoint_file_name + '.json', 'r') as json_file:
 
@@ -14,17 +26,26 @@ def load_endpoint_from_trial(endpoint_file_name, trial_ID_num):
     return endpoint_array[trial_ID_num]
 
 
-def load_endpoints_from_trial(placebo_RR50_array_file_name, placebo_MPC_array_file_name, 
+def load_data_points_from_trial(placebo_RR50_array_file_name, placebo_MPC_array_file_name, 
                               drug_RR50_array_file_name,    drug_MPC_array_file_name,
                               RR50_p_values_filename,       MPC_p_values_filename,
                               trial_ID_num,                 num_patients_per_arm_file_name):
+    '''
 
-    placebo_RR50 = load_endpoint_from_trial(placebo_RR50_array_file_name, trial_ID_num)
-    placebo_MPC  = load_endpoint_from_trial(placebo_MPC_array_file_name,  trial_ID_num)
-    drug_RR50    = load_endpoint_from_trial(drug_RR50_array_file_name,    trial_ID_num)
-    drug_MPC     = load_endpoint_from_trial(drug_MPC_array_file_name,     trial_ID_num)
-    RR50_p_value = load_endpoint_from_trial(RR50_p_values_filename,       trial_ID_num)
-    MPC_p_value  = load_endpoint_from_trial(MPC_p_values_filename,        trial_ID_num)
+    Inputs:
+
+        1)
+
+    Outputs:
+
+    '''
+
+    placebo_RR50 = load_data_point_from_trial(placebo_RR50_array_file_name, trial_ID_num)
+    placebo_MPC  = load_data_point_from_trial(placebo_MPC_array_file_name,  trial_ID_num)
+    drug_RR50    = load_data_point_from_trial(drug_RR50_array_file_name,    trial_ID_num)
+    drug_MPC     = load_data_point_from_trial(drug_MPC_array_file_name,     trial_ID_num)
+    RR50_p_value = load_data_point_from_trial(RR50_p_values_filename,       trial_ID_num)
+    MPC_p_value  = load_data_point_from_trial(MPC_p_values_filename,        trial_ID_num)
 
     with open( os.getcwd() + '/' + num_patients_per_arm_file_name + '.json', 'r') as json_file:
 
@@ -57,12 +78,12 @@ def plot_trial_endpoints(placebo_RR50, placebo_MPC, drug_RR50, drug_MPC, RR50_p_
     ax2.set_yticklabels([0, 10, 20, 30, 40, 50, 60], fontsize = 14)
     ax2.set_ylim([0, 80])
     ax2.set_ylabel('Median Percentage Change', fontsize = 14)
-    longTitle = 'Median Percentage Change Results of One Trial (p' + MPCPValueString + ')'
+    longTitle = 'Median Percentage Change Results of One Trial (p' + str( np.round( MPC_p_value, p_value_decimal_round ) ) + ')'
     formatted_title = '\n'.join(textwrap.wrap(longTitle, 45))
     ax2.set_title(formatted_title, fontsize = 14)
     for rect in rects2:
         height = rect.get_height()
-        ax2.text(rect.get_x() + rect.get_width()/2, 1.05*height, '%g'%np.round(height, genValueRounding), ha = 'center', va = 'bottom', fontsize = 14)
+        ax2.text(rect.get_x() + rect.get_width()/2, 1.05*height, '%g'%np.round(height, endpoint_decimal_round), ha = 'center', va = 'bottom', fontsize = 14)
     
     left  = -0.4  # the left side of the subplots of the figure
     right = 1.3    # the right side of the subplots of the figure
@@ -76,10 +97,26 @@ def plot_trial_endpoints(placebo_RR50, placebo_MPC, drug_RR50, drug_MPC, RR50_p_
     fig.savefig(fname = os.getcwd() + '/Romero-fig5.png', dpi = 600, bbox_inches = 'tight')
 
 
-placebo_RR50_array_file_name = 'placebo_RR50_array'
-placebo_MPC_array_file_name = 'placebo_MPC_array'
-drug_RR50_array_file_name = 'drug_RR50_array'
-drug_MPC_array_file_name = 'drug_MPC_array'
-RR50_p_values_filename = 'RR50_p_values'
-MPC_p_values_filename = 'MPC_p_values'
-num_patients_per_arm_file_name = 'num_patients_per_arm'
+if(__name__ == '__main__'):
+
+    trial_ID_num = int(sys.argv[1])
+
+    placebo_RR50_array_file_name = 'placebo_RR50_array'
+    placebo_MPC_array_file_name = 'placebo_MPC_array'
+    drug_RR50_array_file_name = 'drug_RR50_array'
+    drug_MPC_array_file_name = 'drug_MPC_array'
+    RR50_p_values_filename = 'RR50_p_values'
+    MPC_p_values_filename = 'MPC_p_values'
+    num_patients_per_arm_file_name = 'num_patients_per_arm'
+
+    endpoint_decimal_round = 1
+    p_value_decimal_round = 2
+
+    [placebo_RR50, placebo_MPC, drug_RR50, drug_MPC, RR50_p_value, MPC_p_value, num_patients_per_arm] = \
+        load_data_points_from_trial(placebo_RR50_array_file_name, placebo_MPC_array_file_name, 
+                                    drug_RR50_array_file_name,    drug_MPC_array_file_name,
+                                    RR50_p_values_filename,       MPC_p_values_filename,
+                                    trial_ID_num,                 num_patients_per_arm_file_name)
+
+    plot_trial_endpoints(placebo_RR50, placebo_MPC, drug_RR50, drug_MPC, RR50_p_value, MPC_p_value, 
+                         num_patients_per_arm, endpoint_decimal_round, p_value_decimal_round)
